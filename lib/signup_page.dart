2548e3304ef2c32f7b739d6f751d8c57a8b6eb4a
fenan-yosef <fenanyosef@gmail.com>
2025-05-31 14:49:41 +0300

    Add login and signup page implementations

---
+++ lib/signup_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // TODO: Add email, password, and confirm password fields
+              // TODO: Add signup button
+              // TODO: Add navigation to login page
+              const Text('Sign Up Page'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
